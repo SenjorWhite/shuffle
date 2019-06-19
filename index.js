658cb10f72b4bda3ef430447bb69d1b393dbb65c
senjorwhite <senjorwhite@gmail.com>
2019-06-18 22:47:32 -0600

    Added: Arguments with -p -n

--- index.js
+++ index.js
@@ -1,35 +1,81 @@
-const readline = require("readline");
 const shuffle = require("./src/shuffle");
-
-let rl = readline.createInterface({
+const readline = require("readline").createInterface({
     input: process.stdin,
     output: process.stdout
 })
 
-rl.question("Please input an Integer (>0):", (input) => {
-    if (verifyInputFormat(input)) {
+const flag = {
+    PureArray: false,
+    NoBracket: false,
+    Number: null
+}
+
+loadArguments();
+
+if (flag.Number) {
+    outputResult(flag.Number);
+} else {
+    readline.question("Please input an Integer (>0):", (input) => {
         outputResult(input);
-    } else {
-        notify();
+        readline.close();
+    })
+}
+
+function loadArguments(){
+    for (let i = 2; i < process.argv.length; i++) {
+        let argv = process.argv[i];
+        switch (argv) {
+            case "-p":
+                flag.PureArray = true;
+                break;
+            case "-n":
+                flag.NoBracket = true;
+                break;
+            default:
+                updateNumber(argv);
+        }
     }
-    rl.close();
-})
+}
 
 function outputResult(number) {
-    let randomArray = shuffle.generate(number);
-    if (randomArray === null) {
-        notify();
+    let isValid = verifyInputFormat(number);
+    if (isValid) {
+        let randomArray = shuffle.generate(number);
+        if (randomArray === null) { //case: zero
+            notify();
+        } else {
+            outputWithFormat(randomArray);
+        }
     } else {
-        console.log("Output:\n[%s]", randomArray.toString());
+        notify();
     }
+}
+
+function outputWithFormat(array) {
+    let output = array.toString();
+    if (!flag.NoBracket) {
+        output = "[" + output + "]";
+    }
+    if (!flag.PureArray) {
+        output = "Output:\n" + output;
+    }
+    console.log(output);
+    process.exit(0);
 }
 
 function notify() {
     console.log("Please input a Non-Zero Positive Integers!");
+    process.exit(0);
 }
 
 function verifyInputFormat(input) {
     let reg = /^\d*$/;
     let isValid = reg.test(input);
     return isValid;
+}
+
+function updateNumber(input) {
+    if (verifyInputFormat(input)) {
+        flag.Number = input;
+    }
 }
